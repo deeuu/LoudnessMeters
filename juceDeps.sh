43852648d0b79bfcf71f296d03c70bfea9782d3c
deeuu <contactdominicward@gmail.com>
2014-11-18 12:42:19 +0000

    Added script for installing dependencies

---
+++ juceDeps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get -y install g++
+sudo apt-get -y install libfreetype6-dev
+sudo apt-get -y install libx11-dev
+sudo apt-get -y install libxinerama-dev
+sudo apt-get -y install libxcursor-dev
+sudo apt-get -y install mesa-common-dev
+sudo apt-get -y install libasound2-dev
+sudo apt-get -y install freeglut3-dev
+sudo apt-get -y install libxcomposite-dev
