5e19bf72ce61d83e74ea55be72f723efae4b3951
deeuu <contactdominicward@gmail.com>
2015-09-16 21:03:22 +0100

    No better to keep headers local

---
+++ src/LoudnessMeterCH2012/JuceLibraryCode/modules/juce_video/juce_video.h
@@ -0,0 +1,11 @@
+// This is an auto-generated file to redirect any included
+// module headers to the correct external folder.
+
+#if defined (JUCER_LINUX_MAKE_7346DA2A)
+ #include "../../../../JuceLibraryCode/modules/juce_video/juce_video.h"
+#elif defined (JUCER_VS2013_78A5020)
+ #include "../../../../../juce/juce_video/juce_video.h"
+#else
+ #error "This file is designed to be used in an Introjucer-generated project!"
+#endif
+
---
+++ src/LoudnessMeterGM2002/JuceLibraryCode/modules/juce_audio_plugin_client/juce_audio_plugin_client.h
@@ -0,0 +1,11 @@
+// This is an auto-generated file to redirect any included
+// module headers to the correct external folder.
+
+#if defined (JUCER_LINUX_MAKE_7346DA2A)
+ #include "../../../../JuceLibraryCode/modules/juce_audio_plugin_client/juce_audio_plugin_client.h"
+#elif defined (JUCER_VS2013_78A5020)
+ #include "../../../../../juce/juce_audio_plugin_client/juce_audio_plugin_client.h"
+#else
+ #error "This file is designed to be used in an Introjucer-generated project!"
+#endif
+
